02310694af51362387c984dee833a0870eee711c
Chris Holdgraf <choldgraf@berkeley.edu>
2020-01-15 14:34:29 -0800

    first commit for jupyterbook theme

---
+++ docs/index.rst
@@ -0,0 +1,11 @@
+========
+My index
+========
+
+A landing page that simply links out to two top-level sections to see how
+the sidebar looks.
+
+.. toctree::
+
+    content/features/index.rst
+    content/test_pages/index.rst
---
+++ setup.py
@@ -0,0 +1,26 @@
+"""Sphinx Bootstrap Theme package."""
+from setuptools import setup
+
+
+setup(
+    name="sphinx-jupyter-book-theme",
+    version="0.0.1.dev0",
+    description="Sphinx Bootstrap Theme - pandas version.",
+    url="https://github.com/pandas-dev/pandas-sphinx-theme",
+    #
+    packages=["sphinx_jupyter_book_theme"],
+    package_data={
+        "sphinx_jupyter_book_theme": [
+            "theme.conf",
+            "*.html",
+            "static/sphinx-bootstrap.css_t",
+            "static/css/*.css",
+            "static/js/*.js",
+            "static/img/*",
+        ]
+    },
+    include_package_data=True,
+    # See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
+    entry_points={"sphinx.html_themes": ["sphinx_jupyter_book_theme = sphinx_jupyter_book_theme"]},
+    install_requires=["sphinx"],
+)
